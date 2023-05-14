aa5dfab8d086170f28b721654e8a772dce753844
HandleNine <1085153346@qq.com>
2023-05-14 19:54:06 +0800

    init

---
+++ src/main/java/com/example/bitjumppms/BitJumpPmSApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitjumppms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitJumpPmSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitJumpPmSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bitjumppms/BitJumpPmSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bitjumppms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitJumpPmSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
