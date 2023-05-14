b12f40b2e43195ccbddb072445219505822e6dc9
HandleNine <1085153346@qq.com>
2023-05-14 20:05:35 +0800

    init

--- src/main/java/com/example/bitjumppms/domain/MyUser.java
+++ src/main/java/com/example/bitjumppms/domain/MyUser.java
@@ -1,5 +1,10 @@
 package com.example.bitjumppms.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MyUser {
     private int userId;//用户id
     private int priv;//权限
@@ -22,109 +27,5 @@ public class MyUser {
         user.fistName = "wen";
         user.lastName = "tan";
         return user;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getPriv() {
-        return priv;
-    }
-
-    public void setPriv(int priv) {
-        this.priv = priv;
-    }
-
-    public String getFistName() {
-        return fistName;
-    }
-
-    public void setFistName(String fistName) {
-        this.fistName = fistName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPost() {
-        return post;
-    }
-
-    public void setPost(String post) {
-        this.post = post;
-    }
-
-    public String getPhotoUrl() {
-        return photoUrl;
-    }
-
-    public void setPhotoUrl(String photoUrl) {
-        this.photoUrl = photoUrl;
-    }
-
-    public String getEntryTime() {
-        return entryTime;
-    }
-
-    public void setEntryTime(String entryTime) {
-        this.entryTime = entryTime;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getProjectTeam() {
-        return projectTeam;
-    }
-
-    public void setProjectTeam(String projectTeam) {
-        this.projectTeam = projectTeam;
-    }
-
-    public String getEducation() {
-        return education;
-    }
-
-    public void setEducation(String education) {
-        this.education = education;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
